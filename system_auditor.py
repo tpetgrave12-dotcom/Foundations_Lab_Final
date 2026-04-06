837b3af7a86db24e5064cfc995de2a3c2cf98797
Trey Petgrave <your-github-email@example.com>
2026-04-05 22:11:05 -0400

    sec: completed session 09 automated system auditor logic

--- system_auditor.py
+++ system_auditor.py
@@ -6,15 +6,21 @@ import os
 print("[*] Initiating System Audit...")
 
 # INSTRUCTION 1: Use subprocess.run() to execute 'ps aux'
-# YOUR CODE HERE:
-
+process_list = subprocess.run(["ps", "aux"], capture_output=True, text=True)
 
 # INSTRUCTION 2: Search the captured output for the malicious process
-# YOUR CODE HERE:
-
-
-# INSTRUCTION 3: If found, create a dictionary and save it to 'security_alert.json'
-# YOUR CODE HERE:
-
+if "unauthorized_cryptominer" in process_list.stdout:
+    
+    # INSTRUCTION 3: If found, create a dictionary and save it to 'security_alert.json'
+    alert_data = {
+        "event": "Unauthorized Process",
+        "severity": "High",
+        "process": "unauthorized_cryptominer"
+    }
+    
+    with open("security_alert.json", "w") as file:
+        json.dump(alert_data, file, indent=4)
+    print("[!] ALERT: Unauthorized process detected. JSON report generated.")
 
 print("[+] Audit Complete.")
+
